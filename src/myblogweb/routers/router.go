09981a1f1553fea7d43f4e5d9ee4667991e97d62
AllanYang <allanyang@juniper.net>
2019-10-12 15:12:47 +0800

    add tag

--- src/myblogweb/routers/router.go
+++ src/myblogweb/routers/router.go
@@ -11,11 +11,12 @@ func init() {
 	//登录
 	beego.Router("/login", &controllers.LoginController{})
 
-	beego.Router("/article/add", &controllers.AddArticleController{})
-
 	//写文章
 	beego.Router("/article/add", &controllers.AddArticleController{})
 	//显示文章内容
 	beego.Router("/article/:id", &controllers.ShowArticleController{})
 	//更新文章
+	beego.Router("article/update",&controllers.UpdateArticleController{})
+	beego.Router("/article/delete", &controllers.DeleteArticleController{})
+	beego.Router("/tags", &controllers.TagsController{})
 }
